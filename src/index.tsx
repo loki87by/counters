d328562a72c46682b3b1159a7aa6b4afc01a12a2
Alexey Akulich <loki1987by@yandex.ru>
2022-07-05 00:05:03 +0300

    setup environement

---
+++ src/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { BrowserRouter /* HashRouter uncomment when need gh-pages deploy && commented BrowserRouter */ } from "react-router-dom";
+import { store } from "./redux/store";
+import App from "./components/App/App";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
